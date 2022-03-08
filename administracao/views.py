7fafb1112c20a05c85787b82eec119b0f1bfb411
Flúvio Lima <fluvio.sistema@gmail.com>
2022-03-07 22:37:34 -0300

    Form Cadastro Serviço

---
+++ administracao/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .forms import ServicoForm
+
+
+# Create your views here.
+
+def cadastrar_servico(request):
+    form_servico = ServicoForm()
+    return render(request, 'servicos/form_servico.html', {"form_servico": form_servico})
+    
